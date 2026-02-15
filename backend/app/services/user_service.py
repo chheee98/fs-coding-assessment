dc8e6e4c8d4a6b4245468aaf7acf3f0d90aae84f
chheee <onnmengchhiv@gmail.com>
2026-02-16 03:28:02 +0700

    Fix: Null email unique constraint

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -22,7 +22,7 @@ class UserService:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Username already registered",
             )
-        if await self.user_repository.get_user_by_email(user_in.email):
+        if user_in.email and await self.user_repository.get_user_by_email(user_in.email):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Email already registered",
